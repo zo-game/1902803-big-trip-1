8222a5af0f880deb5b2de231247e424b755fc4c9
zogame002@gmail.com <zogame002@gmail.com>
2022-03-22 14:03:32 +0500

    module3-task2

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import TripEditEventView from './view/site-edit-events-view';
 import { RenderPosition, render as render } from './render.js';
 import { generateEvent } from './mock/event';
 import ControlsTemplate from './view/trip-controls-template';
+import NoTripEventElement from './view/no-events-view';
 
 const eventCount = 8;
 const events = Array.from({length: eventCount}, generateEvent);
@@ -20,6 +21,7 @@ const renderTripEvent = (eventTripListElement, tripEvent) => {
   const replaceFormToCard = () => {
     eventTripListElement.replaceChild(tripEventComponent.element, eventEditComponent.element);
   };
+
   const onEscKeyDown = (evt) => {
     if(evt.key === 'Escape' || evt.key === 'Esc'){
       evt.preventDefault();
@@ -46,13 +48,23 @@ const controlsTemplate = new ControlsTemplate();
 render(tripControls, controlsTemplate.element, RenderPosition.BEFOREEND);
 render(controlsTemplate.element, new SiteMenuView().element, RenderPosition.AFTERBEGIN);
 
-const siteMain = tripBody.querySelector('.page-main');
-render(siteMain, new TicketTemplate(events[0]).element, RenderPosition.AFTERBEGIN);
+const renderBoard = (boardTemplate, boardEvents) => {
 
-const tripEvents = tripBody.querySelector('.trip-events');
-for(let i = 0; i < eventCount; i++){
-  renderTripEvent(tripEvents, events[i]);
-}
+  if(boardEvents.every((event) => event.isArchive)){
+    render(boardTemplate, new NoTripEventElement().element, RenderPosition.AFTERBEGIN);
+    return;
+  }
+
+  render(boardTemplate, new TicketTemplate(events[0]).element, RenderPosition.AFTERBEGIN);
+
+  const tripEvents = tripBody.querySelector('.trip-events');
+  for(let i = 0; i < eventCount; i++){
+    renderTripEvent(tripEvents, events[i]);
+  }
+
+};
+const siteMain = tripBody.querySelector('.page-main');
+renderBoard(siteMain,events);
 
 const tripFilters = tripBody.querySelector('.trip-controls__filters');
 render(tripFilters, new SiteFiltersView().element, RenderPosition.BEFOREEND);
